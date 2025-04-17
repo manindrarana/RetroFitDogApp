8d353d0ff20a676d2e4341d279700534befcb2cd
manindrarana <ranamanindra9@gmail.com>
2025-04-17 23:29:32 +0100

    updated dogsmodel added data classes

--- app/src/main/java/com/example/retrofitdogapp/model/DogModels.kt
+++ app/src/main/java/com/example/retrofitdogapp/model/DogModels.kt
@@ -1,2 +1,12 @@
 package com.example.retrofitdogapp.model
 
+data class DogBreedsResponse(
+    val message: Map<String, List<String>>,
+    val status: String
+)
+
+data class RandomDogImageResponse(
+    val message: String,
+    val status: String
+)
+
